6e42b89fa7513dc945ffbfa0ef359070f9f64eac
YurleySolimer <yurley.solimer@gmail.com>
2019-05-18 11:20:40 -0400

    php

--- contact.php
+++ contact.php
@@ -2,11 +2,11 @@
  
 $nombre = $ _POST['name'];
 $email = $ _POST['email'];
-$mensaje = $ _POST['Message'];
+$mensaje = $ _POST['message'];
 $para = 'yurley.solimer@gmail.com';
 $titulo = 'Solimer Contact';
  
-$msjCorreo = "Nombre: $name\n E-Mail: $email\n Mensaje:\n $Message";
+$msjCorreo = "Nombre: $name\n E-Mail: $email\n Mensaje:\n $message";
  
 if ($ _POST['submit']) {
 if (mail ($para, $titulo, $msjCorreo)) {
