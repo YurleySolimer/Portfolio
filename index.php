75fb7ae93899b9617584a73c2068c9a394d2a012
YurleySolimer <yurley.solimer@gmail.com>
2019-05-18 11:10:36 -0400

    php

--- index.php
+++ index.php
@@ -1,19 +1,394 @@
-<?php
- 
-$nombre = $ _POST['name'];
-$email = $ _POST['email'];
-$mensaje = $ _POST['message'];
-$para = 'yurley.solimer@gmail.com';
-$titulo = 'Solimer Contact';
- 
-$msjCorreo = "Nombre: $name\n E-Mail: $email\n Mensaje:\n $message";
- 
-if ($ _POST['submit']) {
-if (mail ($para, $titulo, $msjCorreo)) {
-    echo 'El mensaje se ha enviado';
-} else {
-    echo 'Falló el envio';
-    }
-}
- 
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>SOLIMER Portfolio Web Developer</title>
+	
+	<!-- CDN Bootstrap -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+	<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+
+	<!-- CDN Font Awesome -->
+
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+	<!-- Bootstrap Scripts -->
+
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"> </script>
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"> </script>
+
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"> </script>
+
+	<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+	
+	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+	<!-- Tipografia -->
+
+	<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+
+
+	<!-- CSS Styles -->
+
+	<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+
+	<link rel="stylesheet" type="text/css" href="styles.css">
+
+</head>
+
+<body>
+	
+	<!-- NAVIGATION -->
+
+	<nav class="navbar navbar-expand-sm fixed-top bg-white" >
+		<div class="container">
+			<a href="index.html" class="navbar-brand">
+				<img src="img/logo.png" class="logo" width="13%">			
+			</a>
+			<button type="button" class="navbar-toggler" data-toggle = "collapse" data-target = "#navbarCollapse">
+				<span class="navbar-toggler-icon btn-dark">	</span>				
+			</button>
+
+			<div class="collapse navbar-collapse" id="navbarCollapse">
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item">
+						<a href="#home" class="btn-nav ml-4 p-1"> Home </a>
+					</li>
+
+					<li class="nav-item">
+						<a href="#Skills" class="btn-nav ml-4 p-1"> Services </a>
+					</li>
+
+					<li class="nav-item">
+						<a href="#Contact" class="btn-nav ml-4 p-1"> Contact </a>
+					</li>	
+
+					<li class="nav-item">
+						<a href="blog/blog.html" class="btn-nav ml-4 p-1"> Blog </a>
+					</li>				
+				</ul>				
+			</div>			
+		</div>		
+	</nav>
+
+	<!-- HOME -->
+
+	<div id="home">
+		<div class="landing-text">
+			<h2 class="landing-sub" data-aos="fade-right" data-aos-delay="200">
+				Hey there! My name is
+			</h2>
+			<br>
+			<h1 class="text-uppercase landing-title" data-aos="zoom-in" data-aos-delay="800">
+				<strong> Yurley Solimer </strong>
+			</h1>
+			<br>	
+			<h2 class="landing-sub" data-aos="fade-left" data-aos-delay="1050">
+				I'm a <strong> Freelance Web Developer.</strong>
+			</h2>	
+		</div>		
+	</div>
+
+
+	<!-- SKILLS -->
+
+	<section id=Skills>
+		<div class="container p-2">
+			<div class="row">
+				<div class="col-sm-12 skills-text">
+					<br>
+					<h1 class="text-center pt-5" data-aos="fade-down" data-aos-delay="100"> <strong> HOW CAN I HELP YOU? </strong>
+					</h1>
+					<br>
+					<p class="text-center p1 p-2" data-aos="fade-up" data-aos-delay="120"> I'm passionate about web design and development. I like to combine creativity and innovation in each of my works and create websites oriented to the user, in order to provide a good experience. With my skills I can:
+					</p>
+					
+					<p class="text-center p-2" data-aos="fade-up" data-aos-delay="140">
+					Create responsive websites that work on a wide range of devices.
+					Use updated technologies to achieve a quality website.
+					<br> Help with the production of a clean and functional design.
+					Provide support and maintenance to your website to maintain high standards on users.	
+					</p>
+					<br>
+				</div>				
+			</div>
+		</div>
+			</section>
+
+		<div class="container">
+			<div class="row">
+
+				<div class="col-3 py-0 px-1 d-none d-lg-block">
+					<div class="row">
+
+						<div class="col-12 pb-2 mg-1">
+							<div class="card text-white">
+					        <img class="card-img img-fluid" src="img/1.png" alt="">
+					        	<div class="card-img-overlay d-flex">
+					        		<a href="https://github.com/yurleysolimer">
+					        			<span class="badge">Titulo</span> 
+					            		<h6 class="card-title text-white">Aquí escribo la descripción.Aquí escribo la descripción.</h6>
+					          		</a>
+					        	</div>
+					      	</div>
+						</div>
+					</div>
+
+					<div class="row">
+
+						<div class="col-12 pb-2 mg-1">
+							<div class="card text-white">
+					        <img class="card-img img-fluid" src="img/1.png" alt="">
+					        	<div class="card-img-overlay d-flex">
+					        		<a href="https://github.com/yurleysolimer">
+					        			<span class="badge">Titulo</span> 
+					            		<h6 class="card-title text-white">Aquí escribo la descripción.Aquí escribo la descripción.</h6>
+					          		</a>
+					        	</div>
+					      	</div>
+						</div>
+					</div>
+				</div>	
+
+				<div class="col-sm-12 col-md-6  py-0 pl-3 pr-1 featcard">
+	   				<div id="featured" class="carousel slide carousel-fade" data-ride="carousel">
+ 		 
+ 		 				<div class="carousel-inner">
+							<div class="carousel-item active">			  
+								<div class="card text-white">
+		        					<img class="card-img img-fluid" src="img/1.png" alt="">
+		           					<div class="card-img-overlay d-flex linkfeat">
+		          						<a href="https://github.com/yurleysolimer">
+		        							<span class="badge">Tipo de Producto</span> 
+		           						 	<h4 class="card-title text-white">Aquí escribo el Titulo</h4>
+		            						<p class="textfeat" style="display: none;">Aquí escribo la descripción.Aquí escribo la descripción. 
+		            						</p>
+		          						</a>
+		        					</div>
+		     					</div>
+	  						</div>
+	  	
+	  					<div class="carousel-item">			  
+	  						<div class="card text-white">
+				        		<img class="card-img img-fluid" src="img/3.png" alt="">
+				        		<div class="card-img-overlay d-flex linkfeat">
+					          		<a href="https://github.com/yurleysolimer">
+					        			<span class="badge">Tipo de Producto</span> 
+					            		<h4 class="card-title text-white">Aquí escribo el Titulo
+					            		</h4>
+					            		<p class="textfeat" style="display: none;">Aquí escribo la descripción.Aquí escribo la descripción.
+					            		</p>
+					        		</a>
+				        		</div>
+		      				</div>
+	  					</div>
+
+	  					<div class="carousel-item">			  
+	  						<div class="card text-white">
+		        				<img class="card-img img-fluid" src="img/img1.png" alt="">
+		        				<div class="card-img-overlay d-flex linkfeat">
+		          					<a href="https://github.com/yurleysolimer">
+		        						<span class="badge">Tipo de Producto</span> 
+		            					<h4 class="card-title text-white">Aquí escribo el Titulo 
+		            					</h4>
+		            					<p class="textfeat" style="display: none;">Aquí escribo la descripción.Aquí escribo la descripción.
+		            					</p>
+		          					</a>
+		        				</div>
+		      				</div>
+	  					</div>	  	
+	  		  		</div>
+	  			</div>
+			</div>
+
+			<div class="col-3 py-0 px-1 d-none d-lg-block">
+				<div class="row">
+
+					<div class="col-12 pb-2 mg-1">
+						<div class="card text-white">
+				        <img class="card-img img-fluid" src="img/1.png" alt="">
+				        	<div class="card-img-overlay d-flex">
+				        		<a href="https://github.com/yurleysolimer">
+				        			<span class="badge">Titulo</span> 
+				            		<h6 class="card-title text-white">Aquí escribo la descripción.Aquí escribo la descripción.</h6>
+				          		</a>
+				        	</div>
+				      	</div>
+					</div>
+				</div>
+
+				<div class="row">
+
+					<div class="col-12 pb-2 mg-1">
+						<div class="card text-white">
+				        <img class="card-img img-fluid" src="img/1.png" alt="">
+				        	<div class="card-img-overlay d-flex">
+				        		<a href="https://github.com/yurleysolimer">
+				        			<span class="badge">Titulo</span> 
+				            		<h6 class="card-title text-white">Aquí escribo la descripción.Aquí escribo la descripción.</h6>
+				          		</a>
+				        	</div>
+				      	</div>
+					</div>
+				</div>				
+
+			</div>
+		</div>
+	</div>
+
+
+	<!-- Contact -->
+		<section id="Contact" class="bg-light py-5">
+		      <div class="container m-3 p-1">
+		        <div class="row">
+		          <div class="col-sm-7">
+		            <h3 class="texto1"> <strong> CONTACT </strong> </h3>
+		            <p class="texto1">
+		              Ready to get your project started?
+		            </p>
+					<form id="contactform" action="contact.php" method="POST">
+						<div class="input-group mb-3">
+			                <div class="input-group-prepend">
+			                 <i class="fa fa-user input-group-text"></i>
+			                </div>
+			                <input type="text" class="form-control" name="name" placeholder="Name" aria-label="Username" aria-describedby="basic-addon1">
+			              </div>
+
+			              <div class="input-group mb-3">
+			                <div class="input-group-prepend">
+			                  <i class="fa fa-envelope input-group-text"></i>
+			                </div>
+			                <input type="text" class="form-control" name="email" placeholder="Email" aria-label="Username" aria-describedby="basic-addon1">
+			              </div>
+
+			              <div class="input-group mb-3">
+			                <div class="input-group-prepend">
+			                  <i class="fa fa-pencil-alt input-group-text"></i>
+			                </div>
+			                <textarea name="" cols="30" rows="10" placeholder="Message" name="message" class="form-control"></textarea>
+			              </div>
+
+			              <button type="submit" class="btn btn2 text-white">Send</button>
+					</form>
+				</div>
+
+			<!-- Card --> 
+
+          <div class="col-5">
+          	<div class="card card-me m-2 p-2">
+          		<div class="card-header card-me-header">
+          			<img src="img/yur.jpg" class="rounded-circle w-25">
+          			
+          			<h4 class="text-center text-white pt-3"> <strong> YURLEY SOLIMER </strong> </h4> 
+          			<h6 class="p-3"> I wake up everyday telling me that I'm an positive influence to the world. </h6>         			
+          		</div>
+          		<div class="card-body p-1">
+          			<div class="card-text">
+             			 <a data-toggle="collapse" data-target="#about" class="btn"> 
+             			 	<i class="fa fa-angle-double-down"> </i>
+   						</a>
+						<div id="about" class="collapse bg-white">
+							<p class="text-dark p-about ml-1 mr-1"> 
+							I'm from Venezuela, actually study Systems Engineering.
+							<br> HTML, CSS, JS are my skills, with knowledge of frameworks and libraries like Bootstrap and ReactJS.
+							<br> Among others of my knowledge are C ++, Java and SQL database managers.
+							<br> I'm constantly learning and updating the tools I use to develop websites much more effective.
+								
+							</p>
+						</div>
+          			</div>        			
+          		</div>
+          		<div class="card-footer bg-white">
+          			<div class="container card-me-footer">
+          				<div class="row">
+          					<div class="col-sm-6">
+       						
+          					</div>
+          					<div class="col-sm-6">
+          						<img src="img/logo.jpg" class="w-50"> 
+          					</div>          					
+          				</div>          				
+          			</div>
+          			<a href="#" title="blog" class="btn-sm btn-outline-dark"> Visit my Blog </a>  
+          		</div>          		
+          	</div>
+          </div>
+
+        </div>
+      </div>
+    </section>
+
+<!-- Footer -->
+
+    <footer>
+    	<div class="banner-wrapper">
+    		<div class="icon-text">
+    			<ul class="social">
+    				<li>
+    					<a href="https://instagram.com/yurleysolimer/" target="_blank">
+    						<i class="fa fa-instagram"> </i>
+    					</a>
+    				</li>
+   					<li>
+   						<a href="https://github.com/YurleySolimer" target="_blank"> 
+   							<i class="fa fa-github"> </i>
+   						</a>
+   					</li>
+   					<li>
+   						<a href="https://twitter.com/yurleySolimer" target="_blank"> 
+   							<i class="fa fa-twitter"> </i>
+   						</a>
+   					</li>
+   					 <li>
+   						<a href="https://www.linkedin.com/in/yurley-solimer-748a45125/" target="_blank"> 
+   							<i class="fa fa-linkedin"> </i>
+   						</a>
+   					</li>
+
+   					 <li>
+   						<a href="https://www.linkedin.com/in/yurley-solimer-748a45125/" target="_blank"> 
+   							<i class="fa fa-book"> </i>
+   						</a>
+   					</li>   					
+
+
+   				</ul>
+	   		</div>
+       	</div>
+    </footer>
+
+<!-- Copywritte -->
+
+    <footer class="second-footer">
+    	<p>
+    		&copy; 2019 SOLIMER
+    	</p>
+    </footer>
+
+<!-- Scripts -->
+
+	<script>
+		$(document).ready(function(){
+		      $(".linkfeat").hover(
+		        function () {
+		            $(".textfeat").show(500);
+		        },
+		        function () {
+		            $(".textfeat").hide(500);
+		        }
+		    );
+		});
+	</script>
+
+	<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+	<!-- <script src="js/index.js"></script> -->
+	<script>
+			AOS.init();
+</script>
+</body>
+</html>
+
